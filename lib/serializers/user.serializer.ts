db92b40984222a3db5e6d36fd8139f8cd87b972c
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2026-03-25 05:44:16 +0700

    Update user

--- lib/serializers/user.serializer.ts
+++ lib/serializers/user.serializer.ts
@@ -7,8 +7,8 @@ export function serializeUser(user: User) {
     username: user.username,
     fullname: user.fullname,
     alamat: user.alamat,
-    // email: (user.email && ((user.email ?? '') != '')) ? maskEmail(decryption(user.email)) : null,
-    // noHp: (user.noHp && ((user.noHp ?? '') != '')) ? maskPhone(decryption(user.noHp)) : null,
+    email: (user.email && ((user.email ?? '') != '')) ? maskEmail(decryption(user.email)) : null,
+    noHp: (user.noHp && ((user.noHp ?? '') != '')) ? maskPhone(decryption(user.noHp)) : null,
     nik: user.nik,
     userStatus: user?.userActivation?.name,
   };
