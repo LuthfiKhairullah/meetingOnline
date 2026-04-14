632f3fa155e87185f8139429749677ed388d3a43
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2026-04-15 06:02:26 +0700

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -11,6 +11,14 @@ const PUBLIC_ROUTES = [
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/images") ||
+    pathname.startsWith("/favicon.ico")
+  ) {
+    return NextResponse.next();
+  }
+
   // ✅ allow public
   if (PUBLIC_ROUTES.some((route) => pathname.startsWith(route))) {
     return NextResponse.next();
